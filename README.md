cc5d849a8195241a55625afdf96649d001430d9a
Your Name <victordeleusse@gmail.com>
2024-03-23 19:57:05 +0100

    first try

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # IP_changer_Tor
 Program for changing IP addresses at desired intervals using the TOR network.
+
+## Set-up
+
+1. Install Tor
+2. Start the service ```(brew) service tor (re)start```
+3. Run the command ```tor --hash-password "YOUR_PASSWORD"``` to generate your proper service password
+4. Modify your Tor config. file **/tor/torrc**
+	- uncomment **Control Port 9051**
+	- uncomment and modify **HashedControlPassword**
+
+## Run
+
+* Start running **toripatables.py** to route all services and traffic including DNS through the tor network.
+
+* Get into *mon_env* by running **source mon_env/bin/activate** and ensure that **stem** package is well installed. Then run **ip_changer.py**
---
+++ prog/ip_changer.py
@@ -0,0 +1,61 @@
+import argparse
+from subprocess import getoutput
+from json import load
+from random import random
+from time import sleep
+from urllib.error import URLError
+from urllib.request import urlopen
+from _thread import start_new_thread
+from stem import Signal
+from stem.control import Controller
+
+class Switcher:
+    def __init__(self, host, port, password, time_interval):
+        self.host = host
+        self.port = port
+        self.password = password
+        self.time_interval = time_interval
+        self.ident = random()
+
+    def start(self):
+        print("TOR Switcher starting.")
+        start_new_thread(self.newnym, ())
+
+    def newnym(self):
+        key = self.ident
+        try:
+            with Controller.from_port(address=self.host, port=self.port) as controller:
+                controller.authenticate(password=self.password)
+                print("AUTHENTICATE accepted.")
+                
+                while key == self.ident:
+                    controller.signal(Signal.NEWNYM)
+                    print("Sent signal NEWNYM")
+
+                    try:
+                        my_new_ident = load(urlopen("https://check.torproject.org/api/ip"))["IP"]
+                    except (URLError, ValueError):
+                        my_new_ident = getoutput("wget -qO - ifconfig.me")
+
+                    print(f"Your IP is {my_new_ident}")
+                    sleep(self.time_interval)
+        except Exception as e:
+            print(f"There was an error here: {e}")
+            import traceback
+            traceback.print_exc()
+            print("Quitting.")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="TOR IP Switcher")
+    parser.add_argument("--host", default="127.0.0.1", help="The host of the TOR service")
+    parser.add_argument("--port", default=9051, type=int, help="The port number of the TOR service")
+    parser.add_argument("--password", required=True, help="The password for the TOR service")
+    parser.add_argument("--time", default=30.0, type=float, help="Time interval between IP switches")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    switcher = Switcher(args.host, args.port, args.password, args.time)
+    switcher.start()
+    input("Press Enter to exit...")
+
